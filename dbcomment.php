dc1ed32a0112e34440cb8cbe984842456957a11d
ubuntu <vinaykumar1989@gmail.com>
2012-12-14 11:59:56 +0530

    to insert comments

--- dbcomment.php
+++
@@ -1,4 +0,0 @@
-<?php
-$conn1=mysql_connect("mysql9.000webhost.com","a4313936_kumar","transcend89");
-mysql_select_db("a4313936_comm",$conn1);
-?>
---
+++ insertnonsense.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+$checkname=$_POST['id'];
+$namme=$_SESSION['xplo1'];
+if(!isset($_SESSION['xplo1']))
+{header("url=http://xplorers.host56.com");}
+else
+{require('dbconnect.php');
+$timepass=$_POST['textinpu'];
+ $submit=$_POST['submit'];
+ echo($timepass);
+ if($submit)
+ {
+    if($timepass)
+    { $cmmname=mysql_fetch_assoc(mysql_query("SELECT nstable FROM xplomembers WHERE uname='$checkname'"));
+	mysql_query("INSERT INTO `{$cmmname['nstable']}` (nsense,coname) VALUES ('$timepass','$namme')");
+	}
+    else
+    {
+        echo("Enter something in Comment Box");
+    }
+ }
+ mysql_close($conn);
+ header("Location: {$_SERVER['HTTP_REFERER']}");
+	}
+	?>
