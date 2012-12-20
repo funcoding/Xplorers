beedf5b0672bb9007ca026abae3f8b99d4e9fd4f
ubuntu <vinaykumar1989@gmail.com>
2012-12-20 23:41:54 +0530

    indented

--- resize.php
+++ resize.php
@@ -1,15 +1,6 @@
 <?php
-
-	// *** Include the class
-	include("resize-class.php");
-
-	// *** 1) Initialise / load image
-	$resizeObj = new resize('sample.jpg');
-
-	// *** 2) Resize image (options: exact, portrait, landscape, auto, crop)
-	$resizeObj -> resizeImage(150, 150, 'crop');
-
-	// *** 3) Save image
-	$resizeObj -> saveImage('sample-resized.jpg', 100);
-
+include("resize-class.php");
+$resizeObj = new resize('sample.jpg');
+$resizeObj->resizeImage(150, 150, 'crop');
+$resizeObj->saveImage('sample-resized.jpg', 100);
 ?>
--- upload1.php
+++ upload1.php
@@ -1,51 +1,29 @@
 <?php
 session_start();
-$usename=$_SESSION['xplo1'];
-if ((($_FILES["file"]["type"] == "image/gif")
-|| ($_FILES["file"]["type"] == "image/jpeg")
-|| ($_FILES["file"]["type"] == "image/pjpeg"))
-&& ($_FILES["file"]["size"] < 2000000))
-  {
-  if ($_FILES["file"]["error"] > 0)
-    {
-    echo "Error: " . $_FILES["file"]["error"] . "<br />";
+$usename = $_SESSION['xplo1'];
+if ((($_FILES["file"]["type"] == "image/gif") || ($_FILES["file"]["type"] == "image/jpeg") || ($_FILES["file"]["type"] == "image/pjpeg")) && ($_FILES["file"]["size"] < 2000000)) {
+    if ($_FILES["file"]["error"] > 0) {
+        echo "Error: " . $_FILES["file"]["error"] . "<br />";
+    } else {
+        move_uploaded_file($_FILES["file"]["tmp_name"], $_FILES["file"]["name"]);
+        include("resize-class.php");
+        $resizeObj = new resize($_FILES["file"]["name"]);
+        $resizeObj->resizeImage(140, 150, 'exact');
+        $resizeObj->saveImage($_FILES["file"]["name"], 100);
+        rename($_FILES["file"]["name"], $usename . ".jpg");
+        $patth = "xplmemb/" . $usename . "/" . $usename . ".jpg";
+        require('dbconnect.php');
+        mysql_query("UPDATE xplomembers SET upict='$patth' WHERE uname='$usename'");
+        mysql_close($conn);
     }
-  else
-    {
-
-	move_uploaded_file($_FILES["file"]["tmp_name"],
-      $_FILES["file"]["name"]);
-	
-		// *** Include the class
-	include("resize-class.php");
-
-	// *** 1) Initialise / load image
-	$resizeObj = new resize($_FILES["file"]["name"]);
-
-	// *** 2) Resize image (options: exact, portrait, landscape, auto, crop)
-	$resizeObj -> resizeImage(140, 150, 'exact');
-
-	// *** 3) Save image
-	$resizeObj -> saveImage($_FILES["file"]["name"], 100);
-	
-     rename($_FILES["file"]["name"],$usename.".jpg");
-	$patth="xplmemb/".$usename."/".$usename.".jpg";
-	
-	require('dbconnect.php');
-	mysql_query("UPDATE xplomembers SET upict='$patth' WHERE uname='$usename'");
-	mysql_close($conn);
-	
+} else {
+    echo "Invalid file";
 }
-  }
-else
-  {
-  echo "Invalid file";
-  }
- require('dbconnect.php');
- $who1=mysql_query("SELECT * FROM xplomembers WHERE uname='$usename'");
-while($som=mysql_fetch_array($who1))
-{$tab=$som['pagepath'];}
+require('dbconnect.php');
+$who1 = mysql_query("SELECT * FROM xplomembers WHERE uname='$usename'");
+while ($som = mysql_fetch_array($who1)) {
+    $tab = $som['pagepath'];
+}
 header("refresh:0,url=http://www.xplorers.host56.com/$tab");
-
 mysql_close($conn);
 ?>
