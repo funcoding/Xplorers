40d139a59e5575e7b18671c941b10b90f55a6a74
ubuntu <vinaykumar1989@gmail.com>
2012-12-23 22:41:57 +0530

    to insert user posts

--- insertcomments.php
+++ insertcomments.php
@@ -2,40 +2,38 @@
 session_start();
 $checkname=$_POST['id'];
 $name=$_SESSION['xplo1'];
+$text=$_POST['textinpu'];
 if(!isset($_SESSION['xplo1']))
 {header("url=http://xplorers.host56.com");}
 else
 {require('dbconnect.php');
-$timepass=strip_tags($_POST['textinpu']);
  $submit=$_POST['submit'];
  if(isset($submit))
  {
-    if(isset($timepass))
+    if(isset($text))
     {
-		$cmmname=$conn->prepare("SELECT `nstable` FROM xplomembers WHERE uname=(?)");
+		$cmmname=$conn->prepare("SELECT `nstable` FROM `xplomembers` WHERE `uname`=(?)");
 		$cmmname->bind_param("s",$checkname);
 		$cmmname->execute();
 		$cmmname->store_result();
 		$cmmname->bind_result($cmmtable);
+		
+		
 			while($cmmname->fetch())
-				{ 
-					$action=$conn->prepare("INSERT INTO `$cmmtable` (`nsense`,`coname`) VALUES (?,?)");
-						if(!$action)
-							{echo("error ".$conn->error);}
-					$action->bind_param("ss",$timepass,$name);
-					$action->store_result();
-					$action->execute();
-				}
+				{
+						$action=$conn->prepare("INSERT INTO `$cmmtable`(`nsense`,`coname`) VALUES (?,?)");
+						if(!$actions)
+							{echo($conn->error);}
+					$action->bind_param("ss",$text,$name);
+					$action->execute();}
+				
+		$conn->close();
 		$cmmname->free_result();
 		$action->free_result();
 	}
-    else
-    {
-        echo("Enter something in Comment Box");
-    }
+   
  }
 
-
- header("Location: {$_SERVER['HTTP_REFERER']}");
+header("Location: {$_SERVER['HTTP_REFERER']}");
 }
 ?>
