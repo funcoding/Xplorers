50fdc0a7d017fbda049c2b5b5428f8779c35015c
ubuntu <vinaykumar1989@gmail.com>
2012-12-29 23:51:08 +0530

    User Profile Page

--- user.php
+++ user.php
@@ -31,24 +31,93 @@ if (!isset($_SESSION['xplo1'])) {
 <title>Xplorers</title>
 <script type="text/javascript" src="http://code.jquery.com/jquery-latest.js"></script>
 <script type="text/javascript" src="http://jzaefferer.github.com/jquery-validation/jquery.validate.js"></script>
-<script src="js/bootstrap.js"></script>
-<link href="css/bootstrap.css" rel="stylesheet"> 
+<script src="/js/bootstrap.js"></script>
+<link href="/css/bootstrap.css" rel="stylesheet"> 
 <h1 ><?php
     if ($checkname == $_SESSION['xplo1']) {
         require("spcheck.php");
     }
 ?></h1>
+
+
+
+
 <script type="text/javascript">
-function des(a,b)
+function deletepost(tableid)
 {
+var page_table="<?php
+    echo ($checkname);
+?>";
+var buttonname=document.getElementById("deletepost_"+tableid);
+var condition=buttonname.getAttribute("name");
+$.ajax({
+	type:"POST",
+	url:"judgement.php",
+	data:{submit:condition,page:page_table,value_table:tableid},
+	success:function(result){
+		alert(result);
+		$("#Post_no_"+tableid).hide();
+	}
+});
+}
+
+</script>
+<script type="text/javascript">
+function insert_reply(postno,commentno)
+{var pagename="<?php
+    echo ($checkname);
+?>";
+    var comment_date="<?php
+    echo (date("d-m", strtotime(now)));
+?>";
+    var loggedin="<?php
+    echo ($_SESSION['xplo1']);
+?>";
+	 var comment= $('#commentarea'+postno).val();
+	$.ajax({
+		type:"POST",
+		url:"judgement.php",
+		dataType: 'json',
+		data:{id:pagename,posttableno:postno,commentss:comment,submit:"insertreply"},
+		success:function(resultreceived)
+		{
+		
+			var html='<li id="subcomment_no_'+resultreceived.id_comment+'" class="media" style=" margin-bottom: 1px;background-color:#EDEFF4;margin-left: 50px;margin-top: 0px;"><img  style="width: 32px; height: 32px; padding-top: 5px; padding-left: 5px; padding-right: 0px; margin-right: 5px;" class="pull-left" style="width: 32px; height: 32px;" alt="http://www.xplorers.host56.com/xplmemb/'+loggedin+'/'+loggedin+'.gif" src="http://www.xplorers.host56.com/xplmemb/'+loggedin+'/'+loggedin+'.jpg"/><div class="media-body"><h6 class="media-heading"> </h6><p style="font-size:12px;word-wrap:break-word;padding-right: 10px;"><strong>'+loggedin+'</strong>&nbsp;'+ resultreceived.comments+ '<input type="button"  id="erase'+resultreceived.id_comment+'" class="btn" style="float:right;"  onClick="javascript:des('+postno+ ',' +resultreceived.id_comment+')" value="erase" /></p><p style="font-size:12px;color:#3B5998;">'+comment_date+'</p></div></li>';
+			
+				$("#commentfieldno"+postno).prepend(html);
+				$('#commentarea'+postno).val('');
+		}
+		
+		
+	
+	});
+}
+
+	
+</script>
+<script type="text/javascript">
+function des(a,b){
+
 var c ="<?php
     echo ($checkname);
 ?>";
-
-window.location="/deletecomment.php?no="+a+"&id="+b+"&user="+c;
-
- }
+	$.ajax({
+  type: "POST",
+  url: "deletecomment.php",
+  dataType: 'json',
+  data: {con:a,id:b,user:c},
+  success:function( msg ) {
+	
+	if(msg[0]=="success")
+	{
+	$("#subcomment_no_"+b).hide();}
+	else
+	{alert("Something Went Wrong");}
+}
+});
+}
 </script>
+
 
 </head>
 <body>
@@ -60,7 +129,7 @@ window.location="/deletecomment.php?no="+a+"&id="+b+"&user="+c;
 		 <div class="nav-collapse"> 
 		 <ul class="nav"> 
 		 <li class="active">
-		<li><a href="">Home</a></li>
+		<li><a href="home.php">Home</a></li>
 		<li><a href="logout.php">Logout</a></li>
 		<li><a href="http://www.xplorers.host56.com/xplmemb/xplorercomments/xplorercomments.html?user=xplorercomments">TimepassBoard</a></li>
 </li>
@@ -69,10 +138,12 @@ window.location="/deletecomment.php?no="+a+"&id="+b+"&user="+c;
 </div>
 </div>
 </div>
+
 <div class="container" style="margin-left: 25px;">
-	
+
 
 <div class="row-fluid">  
+
 <div class="span3">  
           
    
@@ -116,8 +187,10 @@ window.location="/deletecomment.php?no="+a+"&id="+b+"&user="+c;
         <?php
     }
 ?>
-
-
+<p style="padding-left: 10px; word-wrap: break-word; width: 125px;"><?php
+    echo ($checkname);
+?></p>
+    
 <h2 >Friends</h2>
 <div class="thumbnail" style="max-width: 150px; overflow: auto; height: 300px; border-top-width: 0px; border-bottom-width: 0px;padding: 0px;">
 <ul class="nav nav-tabs nav-stacked" style="margin-bottom: 0px;">
@@ -134,7 +207,7 @@ window.location="/deletecomment.php?no="+a+"&id="+b+"&user="+c;
         echo ($hreflink);
 ?>">
 <?php
-        echo ($namedis . " ");
+        echo ($namedis);
 ?>
         </a>
         </li>
@@ -147,12 +220,15 @@ window.location="/deletecomment.php?no="+a+"&id="+b+"&user="+c;
 </ul>
 </div>
 </div>
+
 </div>
 
-<div class="span9">
+<div id="span9" class="span9">
+	
 <div class="thumbnail" style="margin-top: 0px; border-top-width: 0px; border-left-width: 1px; margin-left: -20px; padding-top: 80px; border-right-width: 0px; padding-left: 80px;">
+
 <form class="well" method="post" action="insertcomments.php" style="width: 350px;">
-<label><textarea placeholder="Type something" name="textinpu" style="resize: none; width: 350px; height: 90px; border-right-width: 1px; padding-right: 0px;"></textarea></label>
+<label><textarea style="resize:none" name="textinpu" placeholder="Type something"></textarea></label>
 <input type="submit" class="btn btn-primary "  name="submit" value="post"/> 
 <input type="hidden" value="<?php
 echo ($checkname);
@@ -162,7 +238,9 @@ echo ($checkname);
 require('displaycomments.php');
 ?>
 </div>
+
 </div>
+
 </div>
 </div>
 </body>
