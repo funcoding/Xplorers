eaa8b779e7f79c83e919c224816f11ca0df9963d
ubuntu <vinaykumar1989@gmail.com>
2013-01-28 22:26:44 +0530

    Account Activation

--- confirmaccount.php
+++ confirmaccount.php
@@ -30,34 +30,30 @@
 <?php
 error_reporting(E_ALL);
 require('include/dbconnect.php');
-$email_id=$_GET['email'];
-$check_key=$_GET['activationkey'];
-$activate_user=$conn->prepare("SELECT COUNT(*),memid FROM members WHERE email_address=? AND activation_key=?");
-$activate_user->bind_param("ss",$email_id,$check_key);
+$email_id      = $_GET['email'];
+$check_key     = $_GET['activationkey'];
+$activate_user = $conn->prepare("SELECT COUNT(*),memid FROM members WHERE email_address=? AND activation_key=?");
+$activate_user->bind_param("ss", $email_id, $check_key);
 $activate_user->execute();
-$activate_user->bind_result($count_member,$member_id);
+$activate_user->bind_result($count_member, $member_id);
 $activate_user->store_result();
-while($activate_user->fetch())
-{
-if($count_member!=0)
-{
-	$update_key=$conn->query("UPDATE members SET activation_status=1 WHERE memid=$member_id");
-	if(!$update_key)
-	{echo($conn->error);}
-
-	?>
+while ($activate_user->fetch()) {
+    if ($count_member != 0) {
+        $update_key = $conn->query("UPDATE members SET activation_status=1 WHERE memid=$member_id");
+        if (!$update_key) {
+            echo ($conn->error);
+        }
+?>
 <p>Success.</p>
 <?php
-$activate_user->free_result();
-header('Refresh:1; URL=http://xplorers-appsbyvinay.rhcloud.com');
-
-	}
-else
-{
-	?>
+        $activate_user->free_result();
+        header('Refresh:1; URL=http://xplorers-appsbyvinay.rhcloud.com');
+    } else {
+?>
 <p> Registration Error. </p>
 <?php
-}}
+    }
+}
 ?>
 </fieldset>
 </div>
