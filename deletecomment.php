004c49864408c908674e5b85d1334b1caab7458f
ubuntu <vinaykumar1989@gmail.com>
2013-01-28 22:27:55 +0530

    To delete Posts

--- deletecomment.php
+++ deletecomment.php
@@ -5,8 +5,10 @@ if (isset($_SESSION['userid'])) {
 }
 function deletecomment()
 {
-  
+    $current_user = $_SESSION['xplo1'];
+    $num2         = $_POST['con'];
     $table_to_del = $_POST['id'];
+    $person1      = $_POST['user'];
     require('include/dbconnect.php');
     $query = $conn->prepare("DELETE  FROM `commentsforpost` WHERE `reply_id`=?");
     echo ($conn->error);
